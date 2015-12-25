4e202ad6fef49d4c85adfae65e30b0bb2d919cd6
rekkeb <rekkeb@icloud.com>
2015-12-25 12:07:58 +0100

    Changed [] position

--- eureka-server/src/main/java/com/rekkeb/microservices/eureka/EurekaServer.java
+++ eureka-server/src/main/java/com/rekkeb/microservices/eureka/EurekaServer.java
@@ -11,9 +11,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServer {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
-        new SpringApplicationBuilder((EurekaServer.class)).web(true).run(args);
+        new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
     }
 }
 
